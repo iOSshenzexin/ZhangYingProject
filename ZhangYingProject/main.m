f523830962c917564dbbb8e90edebf66d89019d4
iOSshenzexin <1045250128@qq.com>
2016-07-18 15:56:40 +0800

    Initial Commit

---
+++ ZhangYingProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZhangYingProject
+//
+//  Created by 杨晓婧 on 16/7/18.
+//  Copyright © 2016年 QingDaoShangTong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
