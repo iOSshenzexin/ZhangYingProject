f6b49bcb3b7abb149b41ba15b782540a0efc5c02
iOSshenzexin <1045250128@qq.com>
2016-08-22 17:48:34 +0800

    8.22 解决键盘遮挡

--- ZhangYingProject/Product/Controller/ProductReservationController.h
+++ ZhangYingProject/Product/Controller/ProductReservationController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ProductReservationController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *nameTxt;
+@property (weak, nonatomic) IBOutlet UITextField *idTxt;
+@property (weak, nonatomic) IBOutlet UITextField *dateTxt;
+@property (weak, nonatomic) IBOutlet UITextField *accountTxt;
+@property (weak, nonatomic) IBOutlet UILabel *phoneLbl;
+@property (weak, nonatomic) IBOutlet UILabel *addressLbl;
+@property (weak, nonatomic) IBOutlet UIView *backgorund;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
--- ZhangYingProject/Product/Controller/ProductReservationController.m
+++ ZhangYingProject/Product/Controller/ProductReservationController.m
@@ -10,7 +10,10 @@
 
 #import "DeliveryAddressController.h"
 
-@interface ProductReservationController ()
+@interface ProductReservationController ()<UITextFieldDelegate>{
+    NSInteger prewTag ;  //编辑上一个UITextField的TAG,需要在XIB文件中定义或者程序中添加，不能让两个控件的TAG相同
+    float prewMoveY;
+}
 
 @end
 
@@ -18,6 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.accountTxt.delegate = self;
 }
 
 - (IBAction)didClickChangeAddress:(id)sender {
@@ -26,5 +30,35 @@
     [self.navigationController pushViewController:vc animated:YES];
 }
 
+-(void)textFieldDidBeginEditing:(UITextField *)textField{
+    int offset =  216.0  ;//键盘高度216
+    NSTimeInterval animationDuration = 0.30f;
+    [UIView beginAnimations:@"ResizeForKeyBoard" context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    float width = self.view.frame.size.width;
+    float height = self.view.frame.size.height;
+    if(offset > 0){
+        CGRect rect = CGRectMake(0.0f, -offset,width,height);
+        self.view.frame = rect;
+    }
+    [UIView commitAnimations];
+}
 
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSTimeInterval animationDuration = 0.30f;
+    
+    [UIView beginAnimations:@"ResizeForKeyboard" context:nil];
+    
+    [UIView setAnimationDuration:animationDuration];
+    
+    CGRect rect = CGRectMake(0.0f, 0.0f, self.view.frame.size.width, self.view.frame.size.height);
+    
+    self.view.frame = rect;
+    
+    [UIView commitAnimations];
+    [self.accountTxt resignFirstResponder];
+    
+    return NO;
+}
 @end
